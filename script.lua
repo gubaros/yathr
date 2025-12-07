7339bbb7a4fc0197e80810b4aaa56fb6c8ed1bfd
Guido Barosio <gbarosio@gmail.com>
2025-12-07 15:47:09 -0300

    removing some files

--- script.lua
+++
@@ -1,9 +0,0 @@
--- script.lua
-math.randomseed(os.time())
-
-request = function()
-    local paths = {"/testing", "/asdf", "/google", "/example", "/mach"}
-    local path = paths[math.random(1, #paths)]
-    return wrk.format("GET", path)
-end
-
